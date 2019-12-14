dfe8374d73171d8171dbed43a980f68e3d36f40a
yogeshtambe1994 <yogeshtambe1994@gmail.com>
2019-12-14 17:46:29 +0530

    Changes in adding author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -14,7 +14,7 @@ class AuthorsController < ApplicationController
 
 	def create
 		@author = Author.new(author_params)
-			@author.books << Book.find(params[:author][:book_ids])
+		# @author.books << Book.find(params[:author][:book_ids])
 		if @author.save
 			redirect_to @author
 		else
